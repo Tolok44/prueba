bd23adca043e57f035dc929b820a082012546bdc
David Peña <dechartea19@hotmail.com>
2020-03-13 16:07:22 -0600

    Correccion del template del reporte

--- src/main/java/Utilities/reportMethod.java
+++ src/main/java/Utilities/reportMethod.java
@@ -21,49 +21,49 @@ public class reportMethod {
 		     
 		     printw.println("<html>");
 		     printw.println("<head><title>Reporte</title> "
-		     		+ "<link rel=\"icon\" href=\"../img/Thanos.png\" />\r\n" + 
-		     		"<link rel=\"stylesheet\" href=\"../css/bootstrap.min.css\">\r\n" + 
-		     		"<link rel=\"stylesheet\" href=\"../css/estilos.css\">\r\n" + 
-		     		"<script src=\"../js/jquery-3.4.1.min.js\"></script>\r\n" + 
+		     		+"<link rel=\"icon\" href=\"../img/Thanos.png\" />" + 
+		     		"<link rel=\"stylesheet\" href=\"../css/bootstrap.min.css\">" + 
+		     		"<link rel=\"stylesheet\" href=\"../css/estilos.css\">" + 
+		     		"<script src=\"../js/jquery-3.4.1.min.js\"></script>" + 
 		     		"<script src=\"../js/bootstrap.min.js\"></script>"
 		     		+ "</head>");    
 		     //
 		     printw.println("<body>");
 		    
 		     //Creacion de navbar
-		     printw.println("<nav class=\"navbar-custom\">\r\n" + 
-		     		"    <a class=\"navbar-brand\" href=\"#\">\r\n" + 
-		     		"      <img src=\"../img/iconfinder_JD-27_2624867.png\" width=\"40\" height=\"40\" class=\"d-inline-block align-top\" alt=\"\">\r\n" + 
-		     		"      Thanos Framework\r\n" + 
-		     		"    </a>\r\n" + 
+		     printw.println("<nav class=\"navbar-custom\">" + 
+		     		"    <a class=\"navbar-brand\" href=\"#\">" + 
+		     		"      <img src=\"../img/iconfinder_JD-27_2624867.png\" width=\"40\" height=\"40\" class=\"d-inline-block align-top\">" + 
+		     		"      Thanos Framework" + 
+		     		"    </a>" + 
 		     		"  </nav>");
 	
 			//Creacion de la tabla con sus headers y contenido
-		     printw.println(" <div>\r\n" + 
-		     		"    <table class=\"table table-hover\">\r\n" + 
-		     		"        <thead>\r\n" + 
-		     		"          <tr>\r\n" + 
-		     		"            <th scope=\"col\">No. Step</th>\r\n" + 
-		     		"            <th scope=\"col\">Description</th>\r\n" + 
-		     		"            <th>Action</th>\r\n" + 
-		     		"            <th>Value</th>\r\n" + 
-		     		"            <th scope=\"col\">Status</a></th>\r\n" + 
-		     		"            <th scope=\"col\">Screenshots</th>\r\n" + 
-		     		"            <th scope=\"col\">TimeStamp</th>\r\n" + 
-		     		"          </tr>\r\n" + 
-		     		"        </thead>\r\n"
+		     printw.println(" <div>" + 
+		     		"    <table class=\"table table-hover\">" + 
+		     		"        <thead>" + 
+		     		"          <tr>" + 
+		     		"            <th scope=\"col\">No. Step</th>" + 
+		     		"            <th scope=\"col\">Description</th>" + 
+		     		"            <th>Action</th>" + 
+		     		"            <th>Value</th>" + 
+		     		"            <th scope=\"col\">Status</a></th>" + 
+		     		"            <th scope=\"col\">Screenshots</th>" + 
+		     		"            <th scope=\"col\">TimeStamp</th>" + 
+		     		"          </tr>" + 
+		     		"        </thead>"
 		     		);
 		     for(int i = 0; i < stepList.size(); i++)
 		     {
-		    	 printw.println("<tbody>\r\n" + "<tr>\r\n");
-		    	 printw.println("\"<th scope=\"row\">" + stepList.get(i).step + "</th>\r\n\" + \r\n" + 
-		    	 		"\"<td>" + stepList.get(i).description + "</td>\r\n\" + \r\n" + 
-		    	 		"\"<td>" + stepList.get(i).accion + "</td>\r\n\" + \r\n" + 
-		    	 		"\"<td>" + stepList.get(i).valueAccion + "</td>\r\n\" + \r\n" + 
-		    	 		"\"<td><span><img src=\"../img/pass-icon.png\" height=\"30\" width=\"30\"></span></td>\r\n\" + \r\n" + 
-		    	 		"<td><a href=\"C:\\Users\\Training\\Desktop\\Prueba\\frameWorkBatch3\\Screenshots\\"+stepList.get(i).getTcName()+"\\Step "+ stepList.get(i).step+" "+stepList.get(i).accion +".png\"> Screenshot</a></td>\r\n"+
-		    	 		"\"<td> " + formattedDate + "</td>\"+ \r\n" + 
-		    	 		"\"</tr> </div>\"");
+		    	 printw.println("<tbody>" + "<tr>");
+		    	 printw.println("<th scope=\"row\">" + stepList.get(i).step + "</th>"+ 
+		    	 		"<td>" + stepList.get(i).description + "</td>"+ 
+		    	 		"<td>" + stepList.get(i).accion + "</td>" + 
+		    	 		"<td>" + stepList.get(i).valueAccion + "</td>"+ 
+		    	 		"<td><span><img src=\"../img/pass-icon.png\" height=\"30\" width=\"30\"></span></td>"+ 
+		    	 		"<td><a href=\"C:\\Users\\Training\\Desktop\\Prueba\\frameWorkBatch3\\Screenshots\\"+stepList.get(i).getTcName()+"\\Step "+ stepList.get(i).step+" "+stepList.get(i).accion +".png\"> Screenshot</a></td>"+
+		    	 		"<td> " + formattedDate + "</td>"+ 
+		    	 		"</tr> </div>");
 		     }
 		     printw.println("</body>");
 		     printw.println("</html>");
